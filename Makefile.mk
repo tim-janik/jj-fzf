017fa980e24c68e3b11957b397322466d15f1fa5
Tim Janik <timj@gnu.org>
2025-07-30 03:46:03 +0200

    Makefile.mk: install version.sh next to jj-fzf, hardcode git version
    Signed-off-by: Tim Janik <timj@gnu.org>

--- Makefile.mk
+++ Makefile.mk
@@ -1,21 +1,36 @@
 # This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 
-SHELL	:= /bin/bash -o pipefail
-prefix	 ?= /usr/local
-bindir	 ?= ${prefix}/bin
+SHELL		:= /bin/bash -o pipefail
+version_full	!= ./version.sh
+version_bits    := $(subst _, , $(subst -, , $(subst ., , $(version_full))))
+PREFIX		?= /usr/local
+BINDIR		?= ${PREFIX}/bin
+SHAREDIR	?= $(PREFIX)/share
+PKGVERSION      := $(word 1, $(version_bits)).$(word 2, $(version_bits))
+LIBEXEC		?= libexec/jj-fzf-$(PKGVERSION)
+PRJDIR		?= $(PREFIX)/$(LIBEXEC)
+Q		:= $(if $(findstring 1, $(V)),, @)
+QGEN		 = @echo '  GEN     ' $@
 INSTALL	:= install -c
 RM	:= rm -f
-Q	:= $(if $(findstring 1, $(V)),, @)
+
+
 
 all: check
 
 check-deps: jj-fzf
 	$Q ./jj-fzf --version
 	$Q ./jj-fzf --help >/dev/null # check-deps
-install: check-deps
-	$(INSTALL) jj-fzf "$(bindir)"
+
+install:
+	$(QGEN)
+	mkdir -p $(DESTDIR)$(PRJDIR) $(DESTDIR)$(BINDIR)
+	install -c version.sh jj-fzf $(DESTDIR)$(PRJDIR)
+	@ # Note, .gitattributes:export-subst + git archive + tar are used to hardcode version in $(PRJDIR)/version.sh
+	test ! -e .gitattributes || git archive HEAD version.sh | tar xC $(DESTDIR)$(PRJDIR)
+	ln -sf ../$(LIBEXEC)/jj-fzf $(DESTDIR)$(BINDIR)/jj-fzf
 uninstall:
-	$(RM) "$(bindir)/jj-fzf"
+	rm -r -f $(DESTDIR)$(PRJDIR) $(DESTDIR)$(BINDIR)/jj-fzf
 
 shellcheck-warning: jj-fzf
 	$Q shellcheck --version | grep -q 'script analysis' || { echo "$@: missing GNU shellcheck"; false; }
