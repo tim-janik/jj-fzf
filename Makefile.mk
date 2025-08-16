cb67fb833ca45217816d446db74b114272e3524d
Tim Janik <timj@gnu.org>
2025-08-16 02:23:28 +0200

    Makefile.mk: add Makefile rule to build self-extracting archive for jj-fzf
    This commit introduces a new Makefile rule to generate a self-extracting
    archive (SFX) for jj-fzf. The SFX archive bundles the jj-fzf script with
    all its installation files into a single executable shell script.
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- Makefile.mk
+++ Makefile.mk
@@ -124,6 +124,15 @@ distcheck:
 	$Q echo "Archive ready: artifacts/$(distname).tar.zst" | sed '1h; 1s/./=/g; 1p; 1x; $$p; $$x'
 CLEANDIRS += artifacts
 
+# == artifacts/jj-fzf.sfx ==
+artifacts/jj-fzf.sfx: all
+	$(QGEN)
+	$Q rm -rf xinst/
+	$Q $(MAKE) install DESTDIR=xinst/
+	$Q cd xinst/ && $(abspath sfx.sh) --sfxsh-pack /usr/local/bin/jj-fzf $(abspath $@) *
+	$Q rm -rf xinst/
+	$Q echo "SFX archive ready: $@" | sed '1h; 1s/./=/g; 1p; 1x; $$p; $$x'
+
 # == clean ==
 clean:
 	rm -f $(CLEANFILES)
