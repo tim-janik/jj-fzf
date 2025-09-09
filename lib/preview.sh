0aefa07adf3918e7b552f9cef4f356a17cc8bf32
Tim Janik <timj@gnu.org>
2025-09-09 21:48:02 +0200

    jj-fzf: move op preview, fix evolog preview and evolog paging
    Signed-off-by: Tim Janik <timj@gnu.org>

--- lib/preview.sh
+++ lib/preview.sh
@@ -8,13 +8,14 @@ ABSPATHSCRIPT=$(readlink -f "$0")	# Resolve symlinks to find installdir
 source "${BASH_SOURCE[0]%/*}"/common.sh
 
 # == Preview Rendering ==
-case "${1:-}" in
+# Pattern to match revisions
+REVPAT='^[^a-z()0-9]*([k-xyz]{7,})([?]*)\ '	# line start, ignore --graph, parse revision letters, catch '??'-postfix
+# Pattern to match operation ids
+OPRPAT='^[^a-z0-9]*([0-9a-f]{9,})[?]*\ '	# line start, ignore --graph, parse hex letters, space separator
+case "${1-}" in
   preview_revision)
-    shift
-    # Pattern to match revisions
-    REVPAT='^[^a-z()0-9]*([k-xyz]{7,})([?]*)\ '		# line start, ignore --graph, parse revision letters, catch '??'-postfix
     # Render preview if revision is found
-    if [[ "${*-} " =~ $REVPAT ]] ; then			# match beginning of jj log line
+    if [[ " $2 " =~ $REVPAT ]] ; then			# match beginning of jj log line
       REVISION="${BASH_REMATCH[1]}"
       if [[ "${BASH_REMATCH[2]}" == '??' ]] ; then		# divergent change_id
 	# https://martinvonz.github.io/jj/latest/FAQ/#how-do-i-deal-with-divergent-changes-after-the-change-id
@@ -24,8 +25,37 @@ case "${1:-}" in
       fi
       { jj --no-pager --ignore-working-copy log --color=always --no-graph -T "$JJ_FZF_SHOWDETAILS" -s -r "$REVISION"
 	jj_show_diff --color=always -T '"\n"' -r "$REVISION"
-      } 2>&1 | head -n 2000
+      } 2>&1 | head -n 3000
     fi							# else no valid revision
+    ;;
+  preview_oplog)
+    [[ " $2 " =~ $OPRPAT ]] && {
+      jj --no-pager --ignore-working-copy --at-op "${BASH_REMATCH[1]}" --color=always op log --no-graph -n 1 -T builtin_op_log_comfortable
+      jj --no-pager --ignore-working-copy --at-op "${BASH_REMATCH[1]}" --color=always log -s -r .. # -T builtin_log_oneline
+    }
+    ;;
+  preview_opshow)
+    [[ " $2 " =~ $OPRPAT ]] && {
+      jj --no-pager --ignore-working-copy --at-op "${BASH_REMATCH[1]}" --color=always op log --no-graph -n 1 -T builtin_op_log_comfortable
+      jj --no-pager --ignore-working-copy --at-op "${BASH_REMATCH[1]}" --color=always log --no-graph -s -r "@"
+      jj --no-pager --ignore-working-copy --at-op "${BASH_REMATCH[1]}" --color=always show -T ' "\n" ' -r "@"
+    }
+    ;;
+  preview_oppatch)
+    [[ " $2 " =~ $OPRPAT ]] && {
+      jj --no-pager --ignore-working-copy --color=always op show -p "${BASH_REMATCH[1]}"
+    } | head -n 3000
+    ;;
+  preview_opdiff)
+    [[ " $2 " =~ $OPRPAT ]] && {
+      jj --no-pager --ignore-working-copy --color=always op diff -f "${BASH_REMATCH[1]}" -t @
+    }
+    ;;
+  preview_evolog)
+    [[ " $2 " =~ ' '$BIGHEXPAT' ' ]] && {
+      jj --no-pager --ignore-working-copy evolog --color=always -n1 -p -T 'builtin_log_detailed(commit)' -r "${BASH_REMATCH[1]}" |
+	head -n 3000
+    }
     ;;
 esac
 
