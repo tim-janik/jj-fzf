7fae08f1cf16d01ba665be86732663817af737cf
Tim Janik <timj@gnu.org>
2025-09-11 00:00:10 +0200

    lib/exectool.sh: avoid exit status warnings from jj show --tool
    Signed-off-by: Tim Janik <timj@gnu.org>

--- lib/exectool.sh
+++ lib/exectool.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
-exec $EXECTOOL_CMD "$@"
+$EXECTOOL_CMD "$@" || : # avoid 'Warning: Tool exited with exit status: 1'
 # Hook to serve as a single-executable trampoline for e.g. `EXECTOOL_CMD="git diff" jj show --tool exectool.sh`
