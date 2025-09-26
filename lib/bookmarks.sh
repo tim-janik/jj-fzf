9a4b788d1085e3193c99e7017b9981bec1be653b
Tim Janik <timj@gnu.org>
2025-09-26 22:19:00 +0200

    lib/bookmarks.sh: add explicit handling of (simplified) bookmark states
    Signed-off-by: Tim Janik <timj@gnu.org>

--- lib/bookmarks.sh
+++ lib/bookmarks.sh
@@ -36,6 +36,84 @@ export JJFZF_COMMITID
 jjfzf_status		# snapshot dirty working tree
 B=() H=()
 
+# == Aliases ==
+# Bookmarks are managed locally, @git, @origin and possibly other remotes.
+# There are varying states, depending on wether a bookmark is tracked,
+# new/delete is unpushed, or @remote disagrees with a moved @git after
+# fetch (conflicted).
+# For the UI, we try to map the states of a bookmark name onto 1 dimension,
+# by defining a hierarchy of states for local bookmark names where more
+# important / dominant states shadow others.
+# In addition we list remote bookmarks that have no local name.
+#
+# Possible states:
+# [Deleted]	(but still tracked @origin)
+# [Conflicted]	(local, tracked, undecided @git != @origin)
+# [Tracked]	(local and @origin)
+# [Untracked]	(exists locally and possibly-different @origin)
+# [Local]	(not @origin)
+# [Remote]	(only @origin, not local, untracked)
+cat > $JJFZF_TEMPD/bm.toml <<\__EOF
+[template-aliases]
+# Hierarchical state categories (1D) for a bookmark name
+'bookmark_state1d(untracked)'='''
+if(!present, "Deleted",
+  if(conflict, "Conflicted",
+    if(tracked && remote && remote != "git", "Tracked",
+      if(!tracked && !remote, "Local",
+        if(!tracked && remote != "git", untracked,
+          "OTHER_REMOTE"
+        )
+      )
+    )
+  )
+)'''
+# Format local bookmark with 1D state
+bookmark_local1d='''
+bookmark_state1d("Untracked") ++ '¸'
+++ name ++ '¸' ++ if(remote,"@"++remote) ++ '¸'
+++ pad_end(32, label("bookmark", name), " ") ++ " "
+++ pad_end(13,
+	       label(if(conflict || !present, "conflict"),
+			"[" ++ bookmark_state1d("Untracked") ++ "]") )
+++ if(present && !conflict,
+     format_commit_summary_with_refs(self.normal_target(), "") )
+++ "\n"
+'''
+# Format @remote bookmark with 1D state
+bookmark_remote1d='''
+if(present && !tracked && remote && remote != "git",
+  bookmark_state1d("Remote") ++ '¸'
+  ++ name ++ if(remote,"@"++remote) ++ '¸' ++ if(remote,"@"++remote) ++ '¸'
+  ++ pad_end(32, label("bookmark", name ++ '@' ++ remote), " ") ++ " "
+  ++ pad_end(13, "[" ++ bookmark_state1d("Remote") ++ "]", " ")
+  ++ if(present && !conflict,
+       format_commit_summary_with_refs(self.normal_target(), "") )
+) ++ "\n"
+'''
+# Format tags
+tag_local1d = '''
+"Tag¸"
+++ name ++ '¸@git¸'
+++ pad_end(32, label("tag", name), " ") ++ " "
+++ pad_end(13, "[" ++ "Tag" ++ "]", " ")
+++ format_commit_summary_with_refs(self.normal_target(), "") ++ "\n"
+'''
+__EOF
+
+# == jjfzf_b_l ==
+# jj bookmark list
+jjfzf_b_l()
+(
+  ERR=0
+  [[ " $* " =~ --color ]] && COLOR='' || COLOR="$JJFZF_COLOR"
+  jj --no-pager --ignore-working-copy --config-file=$JJFZF_TEMPD/bm.toml \
+     bookmark list $COLOR "$@" 2>$JJFZF_TEMPD/err || ERR=$?
+  grep -Fv 'Hint:' $JJFZF_TEMPD/err >&2 || :
+  exit $ERR
+)
+export -f jjfzf_b_l
+
 # == Nearest Bookmark ==
 rev_bookmarks()
 {
@@ -45,51 +123,50 @@ rev_bookmarks()
     tr ' ' '\n' | sed -r '/@/d; s/[*?].*//;' || :
 }
 # Identify input bookmark name or find first local bookmark in $INPUT_REV
-NEAREST=( $(jj bookmark list -T 'name++"\n"' -- "$INPUT_REV" | sed -r '1q')
+NEAREST=( $(jjfzf_b_l --color=never -T 'name++"\n"' -- "$INPUT_REV" | sed -r '1q')
 	  $(jj tag list -T 'name++"\n"' -- "$INPUT_REV" | sed -r '1q')
 	  $(rev_bookmarks -t -r "$INPUT_REV")
 	  $(rev_bookmarks -r "$INPUT_REV"-)
 	  $(rev_bookmarks -r "$INPUT_REV"+)
 	  $(rev_bookmarks -r .."$INPUT_REV") )
 
-# == Bookmark & Tag List ==
-jjfzf_format_refs() # width label
+# == jjfzf_bookmark_list0 ==
+jjfzf_bookmark_list0()
 (
-  while read MARK rest ; do
-    MARK="${MARK%:}"
-    printf "%-$1s $2 %s\n" "$MARK" "$rest"
-  done
-)
-jjfzf_bookmark_list()
-{
-  true > $JJFZF_TEMPD/bm_refs.lst
-  # bookmarks tracked @origin
-  jj --no-pager --ignore-working-copy bookmark list --color=never -T 'name++"\n"' -t --remote origin |
-    sort | uniq > $JJFZF_TEMPD/bm_origin.lst
-  # local bookmarks
-  jj --no-pager --ignore-working-copy bookmark list $JJFZF_COLOR -T 'name++"\n"' > $JJFZF_TEMPD/bm_local.lst
-  # loop over local bookmarks to extend formatting
-  while read MARK ; do
-    jj --no-pager --ignore-working-copy bookmark list $JJFZF_COLOR "$MARK" > $JJFZF_TEMPD/bm_1.lst
-    # sed reorders conflicted
-    sed -r ':0; /^\s/!s/ \(conflicted\):/: (conflicted)/; N; $!b0; s/\n\s+/ /g' -i $JJFZF_TEMPD/bm_1.lst
-    grep -qFx "$MARK" $JJFZF_TEMPD/bm_origin.lst &&
-      LABEL="[Bookmark] (tracked)" || LABEL="[Bookmark]"
-    jjfzf_format_refs 40 "$LABEL" < $JJFZF_TEMPD/bm_1.lst >> $JJFZF_TEMPD/bm_refs.lst
-  done < $JJFZF_TEMPD/bm_local.lst
-  # spacer
-  echo				>> $JJFZF_TEMPD/bm_refs.lst
-  # save spacer pos
-  export JJFZF_REFS_START_POS=$(wc -l < $JJFZF_TEMPD/bm_refs.lst)
+  # Keep things simple and only consider remote bookmarks @origin.
+  set -Eeuo pipefail
+  # Truely local bookmark names
+  LOCAL_BOOKMARKS=( $(jjfzf_b_l -a -T 'if(!remote, name) ++ "\n"') )
+  jjfzf_b_l -a -T 'if(!remote || remote == "origin", bookmark_local1d )'  > $JJFZF_TEMPD/bm_local1d
+  for B in "${LOCAL_BOOKMARKS[@]}" ; do
+    for S in "Deleted" "Conflicted" "Tracked" "Untracked" "Local" ; do # "UNKNOWN"
+      grep -m1 "^$S¸$B¸" $JJFZF_TEMPD/bm_local1d && break
+    done
+  done		>  $JJFZF_TEMPD/bm_refs.lst
+  # Bookmarks untracked @origin
+  ORIGIN_BOOKMARKS=( $(jjfzf_b_l --remote origin -T 'if(!tracked && remote == "origin", name) ++ "\n"') )
+  jjfzf_b_l --remote origin -T 'if(!tracked && remote == "origin", bookmark_remote1d )'  > $JJFZF_TEMPD/bm_origin1d
+  for B in "${ORIGIN_BOOKMARKS[@]}" ; do
+    jjfzf_contained "$B" "${LOCAL_BOOKMARKS[@]}" && continue
+    S=Remote
+    grep -m1 "^$S¸$B@[^¸]*¸" $JJFZF_TEMPD/bm_origin1d && break
+  done		>> $JJFZF_TEMPD/bm_refs.lst
+  # Add spacer and save spacer position
+  echo '¸¸¸'	>> $JJFZF_TEMPD/bm_refs.lst
+  wc -l < $JJFZF_TEMPD/bm_refs.lst > $JJFZF_TEMPD/bm_start_pos
   # list tags
-  jj --no-pager --ignore-working-copy tag list $JJFZF_COLOR > $JJFZF_TEMPD/bm_tags.lst
-  # format, adds "[Tag]"
-  jjfzf_format_refs 45 "[Tag]" < $JJFZF_TEMPD/bm_tags.lst	>> $JJFZF_TEMPD/bm_refs.lst
-}
-jjfzf_bookmark_list
+  jj --no-pager --ignore-working-copy --config-file=$JJFZF_TEMPD/bm.toml \
+     tag list $JJFZF_COLOR -T tag_local1d \
+     >> $JJFZF_TEMPD/bm_refs.lst
+  # 0-termination
+  sed '/¸.*¸/s/^/\x00/ ; 1s/^\x00//' < $JJFZF_TEMPD/bm_refs.lst > $JJFZF_TEMPD/bm_refs0.lst
+)
+export -f jjfzf_bookmark_list0
+jjfzf_bookmark_list0	# setup $JJFZF_TEMPD/bm_start_pos
 
 # == Start Position ==
 # Position at nearest bookmark
+JJFZF_REFS_START_POS="$(cat "$JJFZF_TEMPD/bm_start_pos")"
 [[ ${#NEAREST[@]} -ge 1 ]] &&
   NEAREST_POS=$(sed -r 's/\x1b\[[0-9;]*[mK]//g' $JJFZF_TEMPD/bm_refs.lst |
 		  grep -m 1 -n "\b${NEAREST[0]}\b" | cut -d: -f1) &&
@@ -103,6 +180,7 @@ export JJFZF_BOOKMARK_AT=$(jjfzf_jjlog "$JJFZF_LOG_DETAILED_CONFIG" $JJFZF_COLOR
 jjfzf_refs_transform()
 (
   source $JJFZF_TEMPD/bookmarks.env	# MODE
+  [[ "${2-}" == "Tag" ]] && ISTAG=true || ISTAG=false
   AT=$'\n \n '	# place holder for builtin_log_compact
   case "$MODE" in
     T)
@@ -110,14 +188,14 @@ jjfzf_refs_transform()
       ;;
     B)
       T='Set Bookmark'; I='Move or create bookmark'; P='Bookmark Name > '; AT=$' at:\n'"$JJFZF_BOOKMARK_AT"
-      [[ "$1" =~ \[Bookmark\] ]] && G="${1%% *}" || G=''
+      $ISTAG && G='' || G="${1%% *}"
       ;;
     D)
       T='Delete Ref'; I='Delete bookmark or tag'; P='Delete Ref > '; G="${1%% *}";
       ;;
     O)
       T='Track @ Origin'; I='Track origin bookmark'; P='Bookmark to track > '
-      [[ "$1" =~ \[Bookmark\] ]] && G="${1%% *}" || G=''
+      $ISTAG && G='' || G="${1%% *}"
       ;;
     V|*)
       T='View Ref'; I='View details'; P='View Ref > '; G="${1%% *}";
@@ -131,7 +209,7 @@ jjfzf_refs_transform()
   echo -n "+change-footer:${I}$AT"
 )
 export -f jjfzf_refs_transform
-REFRESH='transform(jjfzf_refs_transform {})'
+REFRESH='transform(jjfzf_refs_transform {2} {1})'
 FZF_ARGS+=( --bind "start,focus,change:+$REFRESH" )
 
 # == Bindings ==
@@ -141,7 +219,7 @@ H+=( 'Alt-T: Create new tag' )
 B+=( --bind "alt-t:clear-query+disable-search+search()+execute-silent( sed 's/^MODE=.*/MODE=T/' -i $JJFZF_TEMPD/bookmarks.env )+$REFRESH" )
 H+=( 'Alt-D: Delete bookmark or tag' )
 B+=( --bind "alt-d,ctrl-delete:enable-search+search()+execute-silent( sed 's/^MODE=.*/MODE=D/' -i $JJFZF_TEMPD/bookmarks.env )+$REFRESH" )
-H+=( 'Alt-O: Track bookmark @ origin' )
+H+=( 'Alt-O: Toggle tracking of bookmark @ origin' )
 B+=( --bind "alt-o:enable-search+search()+execute-silent( sed 's/^MODE=.*/MODE=O/' -i $JJFZF_TEMPD/bookmarks.env )+$REFRESH" )
 H+=( 'Alt-V: View details' )
 B+=( --bind "alt-v:execute-silent( echo {q} > $JJFZF_TEMPD/refs_query )+enable-search+execute-silent( sed 's/^MODE=.*/MODE=V/' -i $JJFZF_TEMPD/bookmarks.env )+$REFRESH+transform-query( cat $JJFZF_TEMPD/refs_query )" )
@@ -157,8 +235,8 @@ jjfzf_refs_enter()
   set -Eeuo pipefail
   source $JJFZF_TEMPD/bookmarks.env	# MODE
   QUERY="$2" REF="${1%% *}"
-  [[ "$1" =~ \[Tag\] ]] && ISTAG=true || ISTAG=false
-  [[ "$1" =~ \[Bookmark\] ]] && ISBOOKMARK=true || ISBOOKMARK=false
+  STATE1D="${3-}"
+  [[ "$STATE1D" == "Tag" ]] && ISTAG=true || ISTAG=false
   NEWNAME="$QUERY"
   case "$MODE" in
     T)
@@ -168,47 +246,40 @@ jjfzf_refs_enter()
       }
       ;;
     B)
-      test -z "$NEWNAME" && [[ "$1" =~ \[Bookmark\] ]] && NEWNAME="$REF"
+      test -z "$NEWNAME" -a $ISTAG == false && NEWNAME="$REF"
       test -z "$NEWNAME" -o -z "$JJFZF_COMMITID" || {
 	jjfzf_run +n jj --no-pager bookmark set --allow-backwards -r "$JJFZF_COMMITID" -- "$NEWNAME"
       }
       ;;
     D)
-      $ISBOOKMARK && {
-	jjfzf_run +n jj --no-pager $JJFZF_KEEPCOMMITS bookmark delete "exact:$REF"
-      }
-      $ISTAG && {
+      if [[ "$STATE1D" == "Tag" ]] ; then
 	GIT_DIR=$(jj --no-pager --ignore-working-copy git root) ||
 	  die "need Git to delete tag: $REF"
 	export GIT_DIR
 	jjfzf_run +n git tag -d "$REF"
 	jjfzf_run +n jj --no-pager $JJFZF_KEEPCOMMITS status # import deletion
-      }
+      else
+	jjfzf_run +n jj --no-pager $JJFZF_KEEPCOMMITS bookmark delete "exact:$REF"
+      fi
       ;;
     O)
-      if $ISBOOKMARK ; then
-	ATORIGIN=$(jj --no-pager --ignore-working-copy bookmark list \
-		      -T 'name++if(remote,"@"++remote)++"\n"' -a |
-		     grep -xF "$REF@origin") || :
-	if test -n "$ATORIGIN" ; then	# present @origin
-	  TRACKED=$(jj --no-pager --ignore-working-copy bookmark list \
-		       -T 'name++if(remote,"@"++remote)++"\n"' -t "$REF")
-	  if test -z "$TRACKED" ; then
-	    jjfzf_run +n jj --no-pager bookmark track -- "$REF"@origin
-	  else
-	    jjfzf_run +n jj --no-pager bookmark untrack -- "$REF"@origin
-	  fi
-	else	# needs push to be present @origin
-	  jjfzf_run +n jj git push $JJFZF_COLOR --allow-new --remote origin --bookmark "$REF" --dry-run > $JJFZF_TEMPD/bpush.log 2>&1 \
-	    && STATUS=0 || STATUS=$?
-	  cat $JJFZF_TEMPD/bpush.log
-	  if test $STATUS != 0 || grep -qEi 'nothing *changed|won.?t push|rejected *commit' $JJFZF_TEMPD/bpush.log ; then
-	    read -p "Press Enter..."
-	  else
-	    read -p 'Proceed with bookmark push and submit changes? (y/N) ' YN
-	    [[ "${YN:0:1}" =~ [yY] ]] &&
-	      jjfzf_run +n jj git push $JJFZF_COLOR --allow-new --bookmark "$REF"
-	  fi
+      if [[ "$STATE1D" == "Remote" ]] ; then
+	jjfzf_run +n jj --no-pager bookmark track -- "$REF"
+      elif [[ "$STATE1D" == "Tracked" ]] ; then
+	jjfzf_run +n jj --no-pager bookmark untrack -- "$REF"@origin
+      elif [[ "$STATE1D" == "Untracked" ]] ; then
+	jjfzf_run +n jj --no-pager bookmark track -- "$REF"@origin
+      elif [[ "$STATE1D" == "Local" ]] ; then
+	# needs push to be present @origin
+	jjfzf_run +n jj git push $JJFZF_COLOR --allow-new --remote origin --bookmark "$REF" --dry-run > $JJFZF_TEMPD/bpush.log 2>&1 \
+	  && STATUS=0 || STATUS=$?
+	cat $JJFZF_TEMPD/bpush.log
+	if test $STATUS != 0 || grep -qEi 'nothing *changed|won.?t push|rejected *commit' $JJFZF_TEMPD/bpush.log ; then
+	  read -p "Press Enter..."
+	else
+	  read -p 'Proceed with bookmark push and submit changes? (y/N) ' YN
+	  [[ "${YN:0:1}" =~ [yY] ]] &&
+	    jjfzf_run +n jj git push $JJFZF_COLOR --allow-new --bookmark "$REF"
 	fi
       fi
       ;;
@@ -218,15 +289,15 @@ jjfzf_refs_enter()
 	  jj --no-pager --ignore-working-copy tag list $JJFZF_COLOR -- "exact:$REF"
 	  jj --no-pager --ignore-working-copy bookmark list $JJFZF_COLOR -- "exact:$REF"
 	  echo
-	  REVS="${REF+coalesce( tags(exact:$REF) , bookmarks(exact:$REF) )}"
+	  REVS="${REF+coalesce( tags(exact:'$REF') , bookmarks(exact:'$REF'), $REF )}"
 	  jjfzf_jjlog "$JJFZF_LOG_DETAILED_CONFIG" $JJFZF_COLOR --no-graph -r "$REVS" -p \
 		      -T 'concat( builtin_log_oneline , builtin_log_detailed , diff.stat() , "\n" )'
-	) | $JJFZF_PAGER
+	) |& $JJFZF_PAGER
       ;;
   esac
 )
 export -f jjfzf_refs_enter
-B+=( --bind "enter:execute( jjfzf_refs_enter {} {q} )+transform: source $JJFZF_TEMPD/bookmarks.env && [[ \$MODE != V ]] && echo 'close+close'" )
+B+=( --bind "enter:execute( jjfzf_refs_enter {2} {q} {1} )+transform: source $JJFZF_TEMPD/bookmarks.env && [[ \$MODE != V ]] && echo 'close+close'" )
 
 # == Preview ==
 # Show commit(s) with diff and full info
@@ -237,7 +308,7 @@ jjfzf_ref_info()
 	      -T 'concat( builtin_log_oneline , builtin_log_detailed , diff.stat() , "\n" )'
 )
 export -f jjfzf_ref_info
-B+=( --preview ' L={} && test -n "$L" && jjfzf_ref_info "${L%% *}" ' )
+B+=( --preview ' L={4} && test -n "$L" && jjfzf_ref_info "${L%% *}" ' )
 B+=( --preview-label " Ref Info " )
 
 # == PRINTOUT ==
@@ -255,6 +326,9 @@ FZF_ARGS+=(
   --color=border:red,label:red
   --border-label "-[ ${TITLE^^} — JJ-FZF ]-"
 )
+jjfzf_status
 unset FZF_DEFAULT_OPTS FZF_DEFAULT_COMMAND
+export JJFZF_LOAD_LIST=jjfzf_bookmark_list0
 fzf -m "${FZF_ARGS[@]}" "${B[@]}" \
-    < $JJFZF_TEMPD/bm_refs.lst
+    --read0 '-d¸' --accept-nth=2 --with-nth '{4..}' \
+    < $JJFZF_TEMPD/bm_refs0.lst
