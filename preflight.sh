994d8f55635fb92d6fe5fe92530927b49b8d10b4
Tim Janik <timj@gnu.org>
2026-02-10 19:32:00 +0100

    preflight.sh: bump required jj version to 0.38.0
    Signed-off-by: Tim Janik <timj@gnu.org>

--- preflight.sh
+++ preflight.sh
@@ -79,7 +79,7 @@ command -v "awk" > /dev/null 2>&1 &&
     __preflightish_die "Failed to find usable 'awk' executable in \$PATH"
 
 # == Jujutsu ==
-__preflightish_require "0.37" jj --version --ignore-working-copy
+__preflightish_require "0.38" jj --version --ignore-working-copy
 
 # == fzf ==
 __preflightish_require "0.67.0" fzf --version
